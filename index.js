4ce70adb55e753e7fdb8f305aaba338884d7f1a0
Otto Giron <ottog2486@gmail.com>
2018-12-17 10:20:43 -0600

    add logs to the function

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const Storage = require("@google-cloud/storage").Storage;
 
 exports.metaDataUpdater = (data, context) => {
   const fileInfo = data;
-
+  const cacheMetaDataValue = "max-age=2628000, public";
   const storage = new Storage();
   const bucket = storage.bucket(fileInfo.bucket);
   const file = bucket.file(fileInfo.name);
   const metadata = {
-    cacheControl: "max-age=2628000, public"
+    cacheControl: cacheMetaDataValue
   };
-
-  file.setMetadata(metadata, function(err, apiResponse) {});
+  console.log("Changing file metadata:", fileInfo.name);
+  file.setMetadata(metadata, function(err, apiResponse) {
+    console.log("Changed file "+fileInfo.name+" Cache-control metadata to ", cacheMetaDataValue);
+  });
 };
